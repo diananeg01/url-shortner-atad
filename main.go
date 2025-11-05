788d04532b3dc0f239ba92a189e1b364b1468ba4
diananeg01 <diananegurita07@gmail.com>
2025-11-05 22:16:21 +0200

    test database connection

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"github.com/diananeg01/url-shortner-atad/database"
 	p "github.com/diananeg01/url-shortner-atad/frontend"
 	"net/http"
 )
 
 func main() {
+	database.Init()
+	defer database.Close()
+
+	conn := database.GetDB()
+	var variable any
+
+	conn.QueryRow("SELECT id from test").Scan(&variable)
+	fmt.Println(variable)
+
 	http.HandleFunc("/", p.TestTitle)
 	http.HandleFunc("/line", p.Httpserver)
 
